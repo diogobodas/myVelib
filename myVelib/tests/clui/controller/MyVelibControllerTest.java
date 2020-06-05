cadc1b23836dd24284d12c28caa88721260ab4aa
Bernardo Miranda <bvm@poli.ufrj.br>
2020-06-05 11:30:27 -0300

    tests for clui

---
+++ myVelib/tests/clui/controller/MyVelibControllerTest.java
@@ -0,0 +1,29 @@
+package clui.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyVelibControllerTest {
+
+	@Test
+	void testReadAndWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testParseCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExecuteCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testParseTime() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ myVelib/tests/clui/model/MyVelibModelTest.java
@@ -0,0 +1,54 @@
+package clui.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyVelibModelTest {
+
+	@Test
+	void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTimeWindow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetupString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetupStringIntIntDoubleInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOffline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOnline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRentBike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnBike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlanRide() {
+		fail("Not yet implemented");
+	}
+
+}
